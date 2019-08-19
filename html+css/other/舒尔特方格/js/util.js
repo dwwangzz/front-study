d99c6b43fb5fbc101a7a3428a8196a1b6b46e795
wangzz <81303012@qq.com>
2019-08-19 13:57:58 +0800

    1. 删除不用代码 2. 修改图片路径

--- html+css/other/舒尔特方格/js/util.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.shuffle = function () {
-    for (let t, j, i = this.length; i;) {
-        j = Math.floor(Math.random() * i); // 在前i项中随机取一项，与第i项交换
-        t = this[--i];
-        this[i] = this[j];
-        this[j] = t;
-    }
-}
